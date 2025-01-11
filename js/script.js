ba35e052a97c9b4ebd83289ab5f19cb9a53d41c3
banana <itagues@gmail.com>
2025-01-11 15:33:29 +0100

    “cambios”

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@
 
             // Change navbar background after scrolling 100px
             window.addEventListener('scroll', () => {
-              if (window.scrollY > 100) {
+              if (window.scrollY > 50) {
                     navbar.classList.add('scrolled');
                 } else {
                     navbar.classList.remove('scrolled');
