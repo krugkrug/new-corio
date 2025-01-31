884699e4628cb4f843989f1b7a196528eeb97d89
Sahib <sahibyar0@gmail.com>
2025-01-31 23:41:10 +0500

    site update

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Update button progress on scroll
+window.addEventListener("scroll", () => {
+  const scrollableHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = (window.scrollY / scrollableHeight) * 100;
+  scrollToTopButton.style.setProperty("--scroll", `${scrolled}%`);
+
+  // Show or hide button based on scroll position
+  if (window.scrollY > 200) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+});
+
+// Scroll to top when button is clicked
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Get the current year and update the span footer
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+ 
+//Typing js
+var options = {
+  strings: [
+    "ontinuidad empresarial.",
+    "ontinuidad empresarial.",
+    "ontinuidad empresarial.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+};
+var typed = new Typed(".typing", options);
+
+var options = {
+  strings: ["@corioliscap.com", "@corioliscap.com", "@corioliscap.com"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+};
+var typed = new Typed(".typing2", options);
+
+// Select all dotlottie-player animations
+const lottiePlayers = document.querySelectorAll(".lottie-animation");
+
+// Pause all animations initially
+lottiePlayers.forEach((player) => {
+  player.addEventListener("load", () => {
+    player.pause(); // Pause the animation as soon as it loads
+  });
+});
+
+// Add hover play and pause functionality for each card
+const cards = document.querySelectorAll(".valores__card");
+cards.forEach((card) => {
+  const lottiePlayer = card.querySelector(".lottie-animation");
+
+  card.addEventListener("mouseenter", () => {
+    lottiePlayer.stop(); // Reset animation to the start
+    lottiePlayer.play(); // Play animation on hover
+  });
+});
+
+AOS.init({
+  once: true,
+});
